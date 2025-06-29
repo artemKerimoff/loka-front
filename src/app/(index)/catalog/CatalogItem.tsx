40327a423d0269e97d87be653cba1a605070d057
Артём <99211287+artemKerimoff@users.noreply.github.com>
2025-06-29 15:33:50 +0300

    Update CatalogItem.tsx

--- src/app/(index)/catalog/CatalogItem.tsx
+++ src/app/(index)/catalog/CatalogItem.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { IItem } from "@/shared/data/items.data";
+import Link from "next/link";
 
 // Вспомогательная функция для форматирования цены
 const formatPrice = (price: number) => {
@@ -8,9 +9,9 @@ const formatPrice = (price: number) => {
 
 export function CatalogItem({ item }: { item: IItem }) {
     return (
-        <div className="bg-[#FFFCF7] rounded-2xl p-4 flex flex-col gap-3 transition-shadow hover:shadow-lg cursor-pointer">
-            {/* Контейнер с изображением */}
-            <div className="bg-white rounded-xl flex items-center justify-center p-4 aspect-square">
+      <div className="bg-[#FFFCF7] rounded-2xl p-4 flex flex-col gap-3 transition-shadow hover:shadow-lg cursor-pointer">
+        {/* Контейнер с изображением */}
+        <div className="bg-white rounded-xl flex items-center justify-center p-4 aspect-square">
           <Link href="https://dm09092004.github.io/M/">
             <Image
               src={item.imageUrls[0]}
@@ -22,25 +23,35 @@ export function CatalogItem({ item }: { item: IItem }) {
           </Link>
         </div>
 
-            {/* Контейнер с информацией */}
-            <div>
-                {item.complexName && (
-                    <p className="text-xs text-gray-500 mb-1">{item.complexName}</p>
-                )}
-                <h3 className="font-semibold text-lg">{item.title}</h3>
-            </div>
-
-            {/* Цена и иконка "в избранное" */}
-            <div className="flex justify-between items-center mt-auto pt-2">
-                <p className="text-xl font-bold text-gray-800">
-                    {formatPrice(item.price)} ₽
-                </p>
-                <button className="text-gray-400 hover:text-[#E88246]">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-7 h-7">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.5 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
-                    </svg>
-                </button>
-            </div>
+        {/* Контейнер с информацией */}
+        <div>
+          {item.complexName && (
+            <p className="text-xs text-gray-500 mb-1">{item.complexName}</p>
+          )}
+          <h3 className="font-semibold text-lg">{item.title}</h3>
         </div>
+
+        {/* Цена и иконка "в избранное" */}
+        <div className="flex justify-between items-center mt-auto pt-2">
+          <p className="text-xl font-bold text-gray-800">
+            {formatPrice(item.price)} ₽
+          </p>
+          <button className="text-gray-400 hover:text-[#E88246]">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-7 h-7">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.5 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
     );
 }
